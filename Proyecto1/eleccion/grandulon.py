77301b18cf65f5ee53298239fbaceb8753ddc8c8
andrescollazos <pipecollazos1@gmail.com>
2016-10-06 02:49:43 -0500

    Algoritmo Grandulon: Se implementa el llamado hacia los otros nodos, solo falta que cuando quede el solo se convierta en Coordinador

--- Proyecto1/eleccion/grandulon.py
+++ Proyecto1/eleccion/grandulon.py
@@ -8,24 +8,34 @@ import threading
 TCP_IP = 'localhost'
 TCP_PORT = int(sys.argv[1])#11000
 BUFFER_SIZE = 2048
-
+cambiarCoordinador = False
+contadorProcesos = 0
+#procesosMasGrandes = 0
+# Proceso : [Direccion, puerto, prioridad, estado, esCoordinador?, esElegibleComoCordinador?]
 procesos =[
-			['localhost', 31000, 'active', False],
-			['localhost', 32000, 'active', False],
-			['localhost', 33000, 'active', False]
+			['localhost', 6111, '2', 'active', False, 'ne'],
+			['localhost', 6222, '4', 'active', False, 'ne'],
+			['localhost', 6333, '6', 'inactive', True, 'ne']
 			]
 '''
-			['localhost', 14000, 'active', False],
-			['localhost', 15000, 'active', False],
-			['localhost', 16000, 'active', False],
-			['localhost', 17000, 'active', False],
-			['localhost', 18000, 'inactive', True],
+			['localhost', 14000, '1', 'active', False, 'ne'],
+			['localhost', 15000, '3', 'active', False, 'ne'],
+			['localhost', 16000, '5', 'active', False, 'ne'],
+			['localhost', 17000, '7', 'active', False, 'ne'],
+			['localhost', 18000, '8', 'inactive', True, 'ne'],
 		  ]
 '''
 # ELIMINARSE DE LA LISTA DE PROCESOS:
+estado = ''
+prioridad = 0
+soyCoordinador = False
 for i in procesos:
 	if TCP_PORT in i:
+		estado = i[3]
+		prioridad = i[2]
+		soyCoordinador = i[4]
 		procesos.remove(i)
+
 #SocketServer.ThreadingTCPServer.allow_reuse_address = True
 #s = socket.socket	(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,31 +43,63 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(10)
 
 # MENSAJE TIPO E:
-def escogerCoordinador(IP,Puerto):
-		#global Hour
-		#print "IP: ", IP, " Puerto: ", Puerto
-		#time.sleep(5)
-		s = socket.socket()
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.connect((IP, Puerto))
-		print "Enviare mensaje \"E\" a : ", IP, ":", Puerto
-		s.send("E")
-		time.sleep(1)
-		ok = s.recv(BUFFER_SIZE)
-		time.sleep(1)
-		print ok
-		#Hour = Hour+int(HorRec)
+def escogerCoordinador(IP, Puerto):
+	global cambiarCoordinador
+	global contadorProcesos
+	s = socket.socket()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.connect((IP, Puerto))
+	print "Enviare mensaje \"E\" a : ", IP, ":", Puerto
+	s.send("E")
+	time.sleep(1)
+	ok = s.recv(BUFFER_SIZE)
+	time.sleep(1)
 
-		s.close()
+	if ok == 'OK':
+		print "Recibi mensaje \"OK\", ya no soy elegible como coordinador"
+		cambiarCoordinador = False
+	elif ok == '-1':
+		print "ME llego un mensaje -1"
+		contadorProcesos = contadorProcesos + 1
+	#print ok
+	#Hour = Hour+int(HorRec)
+
+	s.close()
+
+def peticionCoordinador(IP, Puerto):
+	s = socket.socket()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.connect((IP, Puerto))
+	print "Enviare peticion al coordinador: ", IP, ":", Puerto
+	s.send("1")
+	time.sleep(1)
+	activo = s.recv(BUFFER_SIZE)
+	time.sleep(1)
+	print "Estoy ", activo
+	s.close()
+
+	if activo == 'inactive':
+		return "Cambio"
+	else:
+		return "No Cambio"
 
 def connection(sc, addr):
 	#global Segundos, Minutos, Hora
-
+	global cambiarCoordinador
 	Pregunta=sc.recv(BUFFER_SIZE)
 	if Pregunta == "E":
-		print "Enviare respuesta \"OK\" a ", addr[0], ":", addr[1]
-		#HoraEnviada = (Hora*3600)+(Minutos*60)+Segundos
-		sc.send("OK")
+		if estado == 'active':
+			print "ESTOY ACTIVO -> Enviare respuesta \"OK\" a ", addr[0], ":", addr[1]
+			#HoraEnviada = (Hora*3600)+(Minutos*60)+Segundos
+			cambiarCoordinador = True
+			sc.send("OK")
+		else:
+			print "ESTOY INACTIVO -> Enviare respuesta -1 a ", addr[0], ":", addr[1]
+			sc.send(str(-1))
+
+	elif Pregunta == "1":
+		print "Enviare mi estado a ", addr[0], ":", addr[1]
+		sc.send(estado)
 
 
 def Cliente():
@@ -71,19 +113,34 @@ t = threading.Thread(target=Cliente)
 threads.append(t)
 t.start()
 
+# Para llevar a cabo la simulacion del proceso: el proceso 2, el cual tiene el
+# asociado el puerto 31000, identifico
+empezar = True
+value = False
 while True:
 	time.sleep(10)
 
-	if TCP_PORT == 31000:
-		for i in range(len(procesos)):
-			thread.start_new_thread(escogerCoordinador,(procesos[i][0],procesos[i][1]))
+	if TCP_PORT == 6111 and empezar: # Proceso 2
+		for i in procesos:
+			if True in i: # Buscar coordinador
+				cambiar = peticionCoordinador(i[0], i[1])
+				if cambiar == "Cambio":
+					print "Hay que cambiar de coordinador!"
+					cambiarCoordinador = True
+				else:
+					print "El coordinador resolvio mi peticion"
+		empezar = False
 
-	#Hour = Hour + ( ( Hora * 3600 ) + ( Minutos * 60 ) + Segundos )
-	#Hour = Hour / ( len(Clientes) + 1 )
-	#Hora = Hour/3600
-	#Hour = Hour - (Hora*3600)
-	#Minutos = Hour/60
-	#Hour = 0
+	if cambiarCoordinador:
+		procesosMasGrandes = 0
+		for i in procesos:#range(len(procesos)):
+			if prioridad < i[2]:
+				procesosMasGrandes = procesosMasGrandes + 1
+				thread.start_new_thread(escogerCoordinador,(i[0],i[1]))
 
-#if __name__ == '__main__':
-#	main()
+				#print "CAMBIAR COORDINADOR: ", cambiarCoordinador
+		value = True
+	#print "TCP_PORT:", TCP_PORT, "contadorProcesos= ", contadorProcesos
+	if value:
+		if contadorProcesos == procesosMasGrandes:
+			print "Soy el nuevo coordinador! ... :)"
