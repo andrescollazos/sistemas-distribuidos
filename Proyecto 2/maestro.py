06b09c52eb1e92d4bb02f3054a63ec1fd1714920
andrescollazos <pipecollazos1@gmail.com>
2016-11-28 17:17:30 -0500

    Commit 1: Proyecto Final Se cuenta con un nodo maestro, que cumple el metodo dos para encontrar paginas. Se cuenta con sincronización basada en el algoritmo Cristian.
    Se esta trabajando en una estructura que guardara en nodo maestro, la cual consiste en una matriz, una fila por cada uno de los nodos que se van conectando. Cada uno de estos nodos registra su lista de paginsa, en esta pueden haber copias de otras paginas. Es así como se guardará de la siguiente manera:
    
              {Nombre Pagina}  {donde estan su copias}
    Nodo 1 [ [      1-01     ,             2          ],
    Nodo 2 [ [      2-01    ], [          1-01        ], ...]

---
+++ Proyecto 2/maestro.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from socket import AF_INET, SOCK_DGRAM
+import sys
+import socket
+import struct, time
+import thread
+from claseReloj import Reloj
+
+# HORA DESDE SERVIDOR NTP - ACTUANDO COMO CLIENTE
+client = socket.socket(AF_INET, SOCK_DGRAM)
+tiempo_init = time.time()
+client.sendto('\x1b' + 47 * '\0', ("pool.ntp.org", 123))
+msg, address = client.recvfrom(1024)
+tiempo_fin = time.time()
+
+t = struct.unpack("!12I", msg)[10] # CONVERTIR A HORA
+t = int(t - 2208988800L)
+horaI = time.ctime(t).replace(" ", " ")
+print "Hora recibida desde el servidor NTP: ", horaI
+print "Latencia (ms): ", int(1000*(tiempo_fin - tiempo_init))
+
+# AJUSTAR RELOJ LOCAL DEL SERVER
+relojSys = Reloj()
+crearReloj = True
+relojSys.setHora(int(horaI[11]+horaI[12]), int(horaI[14]+horaI[15]), int(horaI[17]+horaI[18]), int(1000*(tiempo_fin - tiempo_init)))
+
+# MATRIZ DE INFORMACIÓN DE LAS PAGINAS
+class Memoria():
+    # CREAMOS UN CLASE MEMORIA QUE NOS PERMITE TENER EL MAPEO DE LAS LISTAS
+    def __init__(self):
+        self.mapa = {}
+
+    # Metodo para imprimir el mapeo
+    def __str__(self):
+        return str(self.mapa)
+
+    # Cuando se registra un nuevo nodo, creamos una llave en la memoria y registramos
+    # sus paginas, si estas paginas no son originales de él, buscamos el propietario
+    # y agregamos una dirección a la copia de la pagina
+    def registrarPaginas(self, nodo, paginas):
+        print "RECORRER LISTA DE PAGINAS DEL NODO", nodo
+        for i in paginas:
+            if not (i[0] == nodo):
+                # Comprar que el nodo con el que se compara si esta creado:
+                try:
+                    print "Soy el nodo: ", i[0]
+                    for j, elem in  enumerate (self.mapa[i[0]]):
+                        print i, "==", elem
+                        if i == elem:
+                            try:
+                                self.mapa[i[0]][j] = elem + "," + nodo
+                                print "todo salio bien"
+                            except:
+                                print "aqui es el error mono"
+                except KeyError:
+                    pass
+                print "Este no es orginal tuyo: ", i
+        self.mapa[nodo] = paginas
+
+    def mostrarMapa(self):
+        while 1:
+            print self
+            time.sleep(3)
+
+
+memoria = Memoria()
+#print "Cree mi memoria: ", memoria
+
+# PREPARAR LOS PUERTOS PARA ESCUCHAR LAS PETICIONES
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', int(sys.argv[1])))
+s.listen(10)
+
+def connection(sc, address, reloj, memoria):
+    #try:
+    data = sc.recv(1024)
+    while True:
+        if data > 0:
+            #print "La hora del servidores es: ", reloj.retHora()
+            data = data.split(";")
+
+            #for i in data[1:]:
+            #    nodo = i[0]
+            #    memoria.mapa.update({nodo:[]})
+            memoria.mapa.update({data[0]:[]})
+            memoria.registrarPaginas(data[0], data[1:])
+            respuesta = reloj.retHora()
+
+            #print memoria
+            sc.send(str(memoria))#respuesta)
+        time.sleep(float(40))
+    #except:
+    #    print "Se termino la conexión con: ", address
+
+while 1:
+    if crearReloj:
+        thread.start_new_thread(relojSys.tick,())
+        thread.start_new_thread(memoria.mostrarMapa, ())
+        crearReloj = False
+    print "A la espera de las conexiones... "
+    sc, addr = s.accept()
+    print "recibida conexion de la IP: " + str(addr[0]) + " puerto: " + str(addr[1])
+    thread.start_new_thread(connection,(sc,addr, relojSys, memoria))
+    print "DESPUES DE RECIBIR CONEXION: ", memoria
+
+sc.close()
+s.close()
